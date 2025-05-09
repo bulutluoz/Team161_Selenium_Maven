035660c1fd336ff97b5c90647d877da718b3569e
bulutluoz <abbulutluoz@gmail.com>
2025-05-09 19:45:55 +0200

    4.ders sonu

---
+++ src/test/java/tests/day04_locators_xpath_cssSelector/C02_ByClassName.java
@@ -0,0 +1,16 @@
+package tests.day04_locators_xpath_cssSelector;
+
+public class C02_ByClassName {
+
+    public static void main(String[] args) {
+
+        //1- Bir test class’i olusturun ilgili ayarlari yapin
+        //2- https://www.testotomasyonu.com/ adresine gidin
+        //3- arama kutusuna phone yazip aratin
+        //4- Category bolumunde 8 element oldugunu test edin
+        //5- Category isimlerini yazdirin
+        //6- Sayfayi kapatin
+
+
+    }
+}
